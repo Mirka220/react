d7525d50ed7803d6b85d7cc2af1b2af5bee4a883
Mirka220 <mirka.gamer@mail.ru>
2022-09-30 08:52:20 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FunctionComponent from './components/FunctionComponent';
 import ClassComponent from './components/ClassComponent';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const changeName = (value) => {
@@ -14,8 +15,20 @@ function App() {
   }
   return (
     <div className="App">
-      <FunctionComponent change={changeName} name="Miroslav" age="17"/>
-      <ClassComponent change={changeAge}name="Vladislav" age="12"/>
+      <nav>
+        <Link to="/class">Class Component</Link>
+        <br/>
+        <Link to="/function">Function Component</Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/class" element={
+        <ClassComponent change={changeAge}name="Vladislav" age="12"/>
+        }/>
+        <Route path="/function" element={
+          <FunctionComponent change={changeName} name="Miroslav" age="17"/>
+        }/>
+      </Routes>
     </div>
   );
 }
